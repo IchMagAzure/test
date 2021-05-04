9a102cb6776f098cb0b9d64c459641cb1be5a5a4
IchMagAzure <83643980+IchMagAzure@users.noreply.github.com>
2021-05-04 12:46:40 +0200

    Genesis

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿# build
+FROM mcr.microsoft.com/dotnet/sdk:5.0.202-alpine3.13-amd64 AS builder
+WORKDIR /app
+COPY . ./
+RUN dotnet publish -c Release -o /out
+
+# run
+FROM mcr.microsoft.com/dotnet/runtime:5.0.5-alpine3.13-amd64
+WORKDIR /app
+COPY --from=builder /out .
+
+# Healthcheck
+RUN apk --no-cache add curl
+HEALTHCHECK --timeout=3s --interval=10s \
+    CMD curl -s --fail http://127.0.0.1:8080/ || exit 1
+
+# User
+USER 1000:1000
+
+ENTRYPOINT ["dotnet", "TestApp1.dll"]
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace TestApp1 {
+	public static class Program {
+		public static void Main(string[] args) {
+			UInt16 port = 8080;
+
+			if (args.FirstOrDefault() is var arg && UInt16.TryParse(arg, out var argPort)) {
+				port = argPort;
+			}
+
+			var listener = new HttpListener() {
+				Prefixes = { $"http://+:{port}/" }
+			};
+
+			listener.Start();
+
+			Console.WriteLine("{0:u} listening at Port {1}", DateTimeOffset.UtcNow, port);
+
+			while (listener.GetContext() is var ctx) {
+				var response = $"{DateTimeOffset.UtcNow:u}\t{ctx.Request.RemoteEndPoint}\t{ctx.Request.LocalEndPoint}";
+				Console.WriteLine(response);
+
+				using var resp = ctx.Response;
+				using var output = resp.OutputStream;
+				using var writer = new StreamWriter(output);
+
+				writer.Write(response);
+			}
+		}
+	}
+}
